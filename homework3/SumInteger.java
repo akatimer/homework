8b88da412f15e474bc3d1d6e6062f54cd7d76bb5
akatimer <akatimer@gmail.com>
2018-01-22 14:15:04 +0200

    Вынесены в отдельный метод операции по вводу одного числа. Метод вызывается дважды.

--- homework3/SumInteger.java
+++ homework3/SumInteger.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class SumInteger {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.print("Введите целое число: ");
-        if (sc.hasNextInt()) {
-            int i = sc.nextInt();
-            System.out.print("Введите второе целое число:");
-            if (sc.hasNextInt()) {
-                int j = sc.nextInt();
-                System.out.println("Сумма введенных вами чисел:" + (i + j));
-            } else {
-                System.out.println("Вы ввели не целое число");
-            }
+        int i =nextInteger();
+        System.out.print("Введите второе целое число: ");
+        int j =nextInteger();
+        System.out.println("Сумма введенных вами чисел " + (i+j));
+    }
+    public static int nextInteger(){
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()){
+            return sc.nextInt();
         } else {
             System.out.println("Вы ввели не целое число");
+            System.exit(2);
         }
+        return 0;
     }
 }
