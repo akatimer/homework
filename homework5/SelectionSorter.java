72723e6ad43820fe876a1f887c54656e3c12fa8c
akatimer <akatimer@gmail.com>
2018-01-22 16:08:05 +0200

    19. Изменить сортировку выбором - исключите обмен значений, если найденный минимальный элемент уже находится на своем месте.

---
+++ homework5/SelectionSorter.java
@@ -0,0 +1,20 @@
+package homework5;
+
+public class SelectionSorter {
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int pos = i;
+            int min = array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    pos = j;
+                    min = array[j];
+                }
+            }
+            if (pos != i) {
+                array[pos] = array[i];
+                array[i] = min;
+            }
+        }
+    }
+}
