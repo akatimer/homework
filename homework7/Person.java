f97a4c98f9d8c95da6fcaba9b8753b14b69751b8
akatimer <akatimer@gmail.com>
2018-01-24 16:43:37 +0200

    Homework 7 1. Class Phone 2. Class Person

---
+++ homework7/Person.java
@@ -0,0 +1,39 @@
+package homework7;
+
+public class Person {
+    String fullName;
+    int age;
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public void move() {
+        System.out.println("Такой то " + fullName + " двигается");
+    }
+
+    public void move(String name) {
+        System.out.println("Такой то " + name + " двигается");
+    }
+
+    public void talk() {
+        System.out.println("Такой то " + fullName + " говорит");
+    }
+
+    public void talk(String name) {
+        System.out.println("Такой то " + name + " говорит");
+    }
+
+    public static void main(String[] args) {
+        Person firstPerson = new Person();
+        Person secondPerson = new Person("Вася", 33);
+        firstPerson.move("Дима");
+        firstPerson.talk("Коля");
+        secondPerson.move();
+        secondPerson.talk();
+    }
+}
