2a785c1e91e6a369fe4a26c878b048e9463b2610
akatimer <akatimer@gmail.com>
2018-01-26 14:00:52 +0200

    Homework 7 1. Class Phone 2. Class Person

--- homework3/SumInteger.java
+++ homework3/SumInteger.java
@@ -5,14 +5,11 @@ import java.util.Scanner;
 public class SumInteger {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введите целое число: ");
-        int i = nextInteger(sc);
-        System.out.print("Введите второе целое число: ");
-        int j = nextInteger(sc);
-        System.out.println("Сумма введенных вами чисел " + (i + j));
+        System.out.println("Сумма введенных вами чисел " + (nextInteger(sc) + nextInteger(sc)));
     }
 
     public static int nextInteger(Scanner sc) {
+        System.out.print("Введите целое число: ");
         if (sc.hasNextInt()) {
             return sc.nextInt();
         } else {
